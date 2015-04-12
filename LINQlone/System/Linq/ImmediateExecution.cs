49ed1a7cbe9531498115ea479c3f9abb8c3be19d
Bedir Yılmaz <3yanlis1bos@gmail.com>
2015-04-12 12:27:30 +0300

    update immediate execution
    minor changes

--- LINQlone/System/Linq/ImmediateExecution.cs
+++ LINQlone/System/Linq/ImmediateExecution.cs
@@ -1,6 +1,6 @@
 ﻿#region Copyright & License Information
 
-// Copyright 2014 Saro Taşciyan, Bedir Yılmaz
+// Copyright 2014 Saro Taşciyan, Bedir Yılmaz 
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
